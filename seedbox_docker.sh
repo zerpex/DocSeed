371c206383bd76f575b6e71e076c8297539302e6
zerpex <zerpex@gmail.com>
2017-03-23 11:30:31 +0100

    [fix] docker install issue

--- seedbox_docker.sh
+++ seedbox_docker.sh
@@ -206,10 +206,10 @@ $SUDO sed -i "s@1069@$GID@g" docker-compose.yml
 
 if [ ! -s /usr/bin/docker ]
 then
-    $SUDO apt-get -y update
-    $SUDO apt-get -y install apt-transport-https ca-certificates curl
     $SUDO apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
     $SUDO echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list
+    $SUDO apt-get -y update
+    $SUDO apt-get -y install apt-transport-https ca-certificates curl
     $SUDO apt-get -y install docker-engine
     $SUDO service docker start
 
