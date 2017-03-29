128d4b4354cdf70c1fffd2e300205fec894d42e3
zerpex <zerpex@gmail.com>
2017-03-29 10:53:16 +0200

    [fix] syncthing default port

--- seedbox_docker.sh
+++ seedbox_docker.sh
@@ -243,7 +243,7 @@ do
             $SUDO sed -i "s@INCOMING@$INC_PATH@g" docker-compose.yml
             $SUDO sed -i "s@MEDIA@$MEDIA_PATH@g" docker-compose.yml
             $SUDO sed -i "s@tool-syncro_Syncthing@$Sy_CNAME@g" docker-compose.yml
-            $SUDO sed -i "s@8384@$Sy_CPORT@g" docker-compose.yml
+            $SUDO sed -i "s@5550@$Sy_CPORT@g" docker-compose.yml
             INSTALLED+=('Sy')
             ;;
     esac
