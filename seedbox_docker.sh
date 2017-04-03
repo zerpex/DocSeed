3d893b7fe108dd409939b0e5bd1842bb7308964c
zerpex <zerpex@gmail.com>
2017-04-03 09:35:14 +0200

    [enh] Add choice on the interface to use for generated links

--- seedbox_docker.sh
+++ seedbox_docker.sh
@@ -232,7 +232,7 @@ do
         13)# Start menu
             cat files/samples/startpage.docker >> docker-compose.yml
             $SUDO sed -i "s@55000@$St_CPORT@g" docker-compose.yml
-	cmd=($DIALOG " --title " Select interface " --clear --radiolist "Please select the network interface to use on the start menu:\n\n " 20 75 5)
+	cmd=($DIALOG --title " Select interface " --clear --radiolist "Please select the network interface to use on the start menu:\n\n " 20 75 5)
 	options=("LAN" ": $LAN" on
 		 "WAN" ": $WAN\n\nWARN : start page is not secure.\n If WAN is enabled, don\'t forget to protect it !" off)
 	choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
