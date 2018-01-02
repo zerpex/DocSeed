9efca553e9f09c568be08f7f341a70b1ee3ba9c3
zerpex <zerpex@gmail.com>
2018-01-02 16:03:56 +0100

    [fix] Updating installation.

--- files/scripts/ins_docker.sh
+++ files/scripts/ins_docker.sh
@@ -2,9 +2,8 @@
 #
 # This script installs docker
 
-    $SUDO apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-    $SUDO echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list
-    $SUDO apt-get -y update
-    $SUDO apt-get -y install apt-transport-https ca-certificates curl
-    $SUDO apt-get -y install docker-engine
+    $SUDO curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add 
+	$SUDO add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+	$SUDO apt-get update
+    $SUDO apt-get -y install docker-ce
     $SUDO service docker start
