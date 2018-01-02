a0304d0e58813aa7730011bca1ac8c63eaf95aa0
zerpex <zerpex@gmail.com>
2018-01-02 21:49:53 +0100

    [fix] FQDN.

--- files/scripts/app_Muximux.sh
+++ files/scripts/app_Muximux.sh
@@ -4,6 +4,7 @@
 
 cat files/includes/muximux.docker >> docker-compose.yml
 
+$SUDO sed -i "s@FQDN@$Mx_SDOM.$DOMAIN@g" docker-compose.yml
 $SUDO sed -i "s@tool-manage_Muximux@$Mx_CNAME@g" docker-compose.yml
 $SUDO sed -i "s@192.168.42.52@$IFACE@g" files/includes/muximux.conf
 
