91fff1d0366fc914c7c76b9b4e8a8cde2f457cd9
zerpex <zerpex@gmail.com>
2018-01-02 21:50:49 +0100

    [fix] FQDN.

--- files/scripts/app_Muximux.sh
+++ files/scripts/app_Muximux.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # Muximux installation
 
-
 cat files/includes/muximux.docker >> docker-compose.yml
 
 $SUDO sed -i "s@FQDN@$Mx_SDOM.$DOMAIN@g" docker-compose.yml
 $SUDO sed -i "s@tool-manage_Muximux@$Mx_CNAME@g" docker-compose.yml
-$SUDO sed -i "s@192.168.42.52@$IFACE@g" files/includes/muximux.conf
+$SUDO sed -i "s@192.168.42.52@$Mx_SDOM.$DOMAIN@g" files/includes/muximux.conf
 
 INSTALLED+=('Mx')
