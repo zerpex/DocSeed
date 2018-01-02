71019f38f36504f7d7e810a2ab4ebabc7c8a7a36
zerpex <zerpex@gmail.com>
2018-01-02 16:05:25 +0100

    [fix] Auto-get latest version.

--- files/scripts/ins_docker-compose.sh
+++ files/scripts/ins_docker-compose.sh
@@ -2,5 +2,6 @@
 #
 # This script installs docker-compose
 
-    $SUDO curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    $SUDO DOCKER_COMPOSE_VERSION="${DOCKER_COMPOSE_VERSION:-$(curl https://github.com/docker/compose/releases | grep releases/tag | grep -v rc | awk -F">" '{print $2}' | awk -F"<" '{print $1}' | head -1)}"
+    $SUDO curl -L https://github.com/docker/compose/releases/download/"$DOCKER_COMPOSE_VERSION"/docker-compose-"$(uname -s)"-"$(uname -m)" > /usr/local/bin/docker-compose
     $SUDO chmod +x /usr/local/bin/docker-compose
