0bf54ddd9601a507bd3943672aae3effa247b139
zerpex <zerpex@gmail.com>
2018-01-02 16:44:25 +0100

    [fix] var name.

--- files/scripts/app_Traefik.sh
+++ files/scripts/app_Traefik.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Traefik installation
 
-mkdir -p $C_PATH/traefik
+mkdir -p $CONF_PATH/traefik
 
 cp files/includes/traefik.toml $CONF_PATH/traefik/traefik.toml
 cat files/includes/traefik.docker >> docker-compose.yml
