222887bf59907ff4054e848346d1bf1ff4dc6a55
zerpex <zerpex@gmail.com>
2018-01-02 16:04:27 +0100

    [fix] indentation.

--- files/scripts/ins_docker.sh
+++ files/scripts/ins_docker.sh
@@ -3,7 +3,7 @@
 # This script installs docker
 
     $SUDO curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add 
-	$SUDO add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-	$SUDO apt-get update
+    $SUDO add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    $SUDO apt-get update
     $SUDO apt-get -y install docker-ce
     $SUDO service docker start
