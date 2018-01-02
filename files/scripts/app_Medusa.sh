f5526ad0314d53b8114656835dd2b1b08e770748
zerpex <zerpex@gmail.com>
2018-01-02 15:06:40 +0100

    [enh] Replace Sickgear by Medusa.

--- files/scripts/app_Medusa.sh
+++ files/scripts/app_Medusa.sh
@@ -5,7 +5,7 @@
 $SUDO mkdir -p $INC_PATH/tv
 $SUDO mkdir -p $MEDIA_PATH/tv
 
-cat files/includes/sickgear.docker >> docker-compose.yml
+cat files/includes/medusa.docker >> docker-compose.yml
 
 $SUDO sed -i "s@INCOMING@$INC_PATH/tv@g" docker-compose.yml
 $SUDO sed -i "s@TVSHOWS@$MEDIA_PATH/tv@g" docker-compose.yml
@@ -14,8 +14,8 @@ $SUDO sed -i "s@autodl-tv_sickgear@$Sg_CNAME@g" docker-compose.yml
 # Set Muximux configuration
 cat <<EOF >> files/includes/muximux.conf
 
-[SickGear]
-name = "SickGear"
+[Medusa]
+name = "Medusa"
 url = "http://192.168.42.52:5005"
 scale = 1
 icon = "muximux-sickbeard"
