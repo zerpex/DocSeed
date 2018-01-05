d30c21cd49a0a4b3e3214eb9c940330aabafe211
zerpex <zerpex@gmail.com>
2018-01-05 14:23:23 +0100

    [enh] Set redis config.

--- files/scripts/app_Nextcloud.sh
+++ files/scripts/app_Nextcloud.sh
@@ -3,7 +3,7 @@
 
 cat files/includes/nextcloud.docker >> docker-compose.yml
 
-$SUDO sed -i "s@DATA@$DEFAULT_PATH/movies@g" docker-compose.yml
+$SUDO sed -i "s@DATA@$DEFAULT_PATH@g" docker-compose.yml
 $SUDO sed -i "s@FQDN@$Nx_SDOM.$DOMAIN@g" docker-compose.yml
 $SUDO sed -i "s@cloud-nextcloud@$Nx_CNAME@g" docker-compose.yml
 
@@ -20,5 +20,17 @@ EOF
 $SUDO sed -i "s@192.168.42.52@$Nx_SDOM.$DOMAIN@g" files/includes/muximux.conf
 
 ## Set conf for redis
+NEXTCLOUD_CONF=$CONF/files/apps/nextcloud/conf/config.php
+sed -i '$ d' $NEXTCLOUD_CONF
+cat >> $NEXTCLOUD_CONF <<- EOM
+'memcache.distributed' => '\OC\Memcache\Redis',
+'memcache.locking' => '\OC\Memcache\Redis',
+'memcache.local' => '\OC\Memcache\APCu',
+'redis' => array(
+   'host' => 'redis',
+   'port' => 6379,
+   ),
+);
+EOM
 
 INSTALLED+=('Nx')
