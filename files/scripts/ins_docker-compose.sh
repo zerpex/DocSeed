3cb58581bc1ae14ec9daf3b4bdd0bb6dbb7ed672
zerpex <zerpex@gmail.com>
2018-01-05 11:34:29 +0100

    [enh] Optimize latest version calculation method.

--- files/scripts/ins_docker-compose.sh
+++ files/scripts/ins_docker-compose.sh
@@ -2,6 +2,6 @@
 #
 # This script installs docker-compose
 
-    $SUDO DOCKER_COMPOSE_VERSION="${DOCKER_COMPOSE_VERSION:-$(curl https://github.com/docker/compose/releases | grep releases/tag | grep -v rc | awk -F">" '{print $2}' | awk -F"<" '{print $1}' | head -1)}"
+    $SUDO DOCKER_COMPOSE_VERSION="$(curl https://github.com/docker/compose/releases/latest 2> /dev/null | sed 's#.*tag/##;s#">.*##')"
     $SUDO curl -L https://github.com/docker/compose/releases/download/"$DOCKER_COMPOSE_VERSION"/docker-compose-"$(uname -s)"-"$(uname -m)" > /usr/local/bin/docker-compose
     $SUDO chmod +x /usr/local/bin/docker-compose
