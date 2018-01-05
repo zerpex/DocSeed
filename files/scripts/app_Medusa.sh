83428bd7514720743f17b2d03c31c72502319528
zerpex <zerpex@gmail.com>
2018-01-05 11:47:34 +0100

    [fix] Container's name.

--- files/scripts/app_Medusa.sh
+++ files/scripts/app_Medusa.sh
@@ -10,7 +10,7 @@ cat files/includes/medusa.docker >> docker-compose.yml
 $SUDO sed -i "s@FQDN@$Sg_SDOM.$DOMAIN@g" docker-compose.yml
 $SUDO sed -i "s@INCOMING@$INC_PATH/tv@g" docker-compose.yml
 $SUDO sed -i "s@TVSHOWS@$MEDIA_PATH/tv@g" docker-compose.yml
-$SUDO sed -i "s@autodl-tv_sickgear@$Sg_CNAME@g" docker-compose.yml
+$SUDO sed -i "s@autodl-tv_medusa@$Sg_CNAME@g" docker-compose.yml
 
 # Set Muximux configuration
 cat <<EOF >> files/includes/muximux.conf
