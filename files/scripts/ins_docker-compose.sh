81fefac8aff6ebda5a7ad93759efcd22cbd6eb48
zerpex <zerpex@gmail.com>
2018-01-24 15:41:37 +0100

    [enh] add compose completion.

--- files/scripts/ins_docker-compose.sh
+++ files/scripts/ins_docker-compose.sh
@@ -4,4 +4,5 @@
 
     $SUDO DOCKER_COMPOSE_VERSION="$(curl https://github.com/docker/compose/releases/latest 2> /dev/null | sed 's#.*tag/##;s#">.*##')"
     $SUDO curl -L https://github.com/docker/compose/releases/download/"$DOCKER_COMPOSE_VERSION"/docker-compose-"$(uname -s)"-"$(uname -m)" > /usr/local/bin/docker-compose
+    $SUDO curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
     $SUDO chmod +x /usr/local/bin/docker-compose
