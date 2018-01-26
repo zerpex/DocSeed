8f569daf0b8cfd87f3cf5c9db7f8d4ef8cb85325
zerpex <zerpex@gmail.com>
2018-01-26 10:32:39 +0100

    [enh] Clean code.

--- files/scripts/ins_docker.sh
+++ files/scripts/ins_docker.sh
@@ -2,8 +2,8 @@
 #
 # This script installs docker
 
-    $SUDO curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add 
-    $SUDO add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-    $SUDO apt-get update
-    $SUDO apt-get -y install docker-ce
-    $SUDO service docker start
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add 
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    apt-get update
+    apt-get -y install docker-ce
+    service docker start
