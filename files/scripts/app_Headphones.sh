93b90a5b207b7aceca2bf6ed735323fe7700c34d
zerpex <zerpex@gmail.com>
2018-01-26 10:26:07 +0100

    [enh] Clean code.

--- files/scripts/app_Headphones.sh
+++ files/scripts/app_Headphones.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # Headphones installation
 
-
-$SUDO mkdir -p $INC_PATH/music
-$SUDO mkdir -p $MEDIA_PATH/sound/music
+mkdir -p $INC_PATH/music
+mkdir -p $MEDIA_PATH/sound/music
 
 cat files/includes/headphones.docker >> docker-compose.yml
 
-$SUDO sed -i "s@FQDN@$Hp_SDOM.$DOMAIN@g" docker-compose.yml
-$SUDO sed -i "s@INCOMING@$INC_PATH/music@g" docker-compose.yml
-$SUDO sed -i "s@ZIC@$MEDIA_PATH/sound/music@g" docker-compose.yml
-$SUDO sed -i "s@autodl-music_headphones@$Hp_CNAME@g" docker-compose.yml
+sed -i "s@FQDN@$Hp_SDOM.$DOMAIN@g" docker-compose.yml
+sed -i "s@INCOMING@$INC_PATH/music@g" docker-compose.yml
+sed -i "s@ZIC@$MEDIA_PATH/sound/music@g" docker-compose.yml
+sed -i "s@autodl-music_headphones@$Hp_CNAME@g" docker-compose.yml
 
 cat <<EOF >> files/includes/muximux.conf
 
@@ -23,6 +22,5 @@ color = "#000000"
 enabled = "true"
 EOF
 
-$SUDO sed -i "s@192.168.42.52@$Hp_SDOM.$DOMAIN@g" files/includes/muximux.conf
-
+sed -i "s@192.168.42.52@$Hp_SDOM.$DOMAIN@g" files/includes/muximux.conf
 INSTALLED+=('Hp')
