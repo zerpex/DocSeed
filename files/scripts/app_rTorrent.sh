432aa70a9df6c01f711cd00d80b620f1402ba7c9
zerpex <zerpex@gmail.com>
2018-01-26 10:31:28 +0100

    [enh] Clean code.

--- files/scripts/app_rTorrent.sh
+++ files/scripts/app_rTorrent.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # rTorrent installation
 
-
 cat files/includes/rtorrent.docker >> docker-compose.yml
 
-$SUDO sed -i "s@FQDN@$Rt_SDOM.$DOMAIN@g" docker-compose.yml
-$SUDO sed -i "s@INCOMING@$INC_PATH@g" docker-compose.yml
-$SUDO sed -i "s@dl-torrent_rtorrent@$Rt_CNAME@g" docker-compose.yml
+sed -i "s@FQDN@$Rt_SDOM.$DOMAIN@g" docker-compose.yml
+sed -i "s@INCOMING@$INC_PATH@g" docker-compose.yml
+sed -i "s@dl-torrent_rtorrent@$Rt_CNAME@g" docker-compose.yml
 
 # Set Muximux configuration
 cat <<EOF >> files/includes/muximux.conf
@@ -20,10 +19,8 @@ color = "#1a1bfe"
 enabled = "true"
 EOF
 
-$SUDO sed -i "s@192.168.42.52@$Rt_SDOM.$DOMAIN@g" files/includes/muximux.conf
-
-$SUDO mkdir -p $CONF_PATH/rtorrent/conf/
-$SUDO cp files/includes/rtorrent.conf $CONF_PATH/rtorrent/conf/.rtorrent.rc
-$SUDO chown -R $SUID:$SGID $CONF_PATH/rtorrent
-
+sed -i "s@192.168.42.52@$Rt_SDOM.$DOMAIN@g" files/includes/muximux.conf
+mkdir -p $CONF_PATH/rtorrent/conf/
+cp files/includes/rtorrent.conf $CONF_PATH/rtorrent/conf/.rtorrent.rc
+chown -R $SUID:$SGID $CONF_PATH/rtorrent
 INSTALLED+=('Rt')
