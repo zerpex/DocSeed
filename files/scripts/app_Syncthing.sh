83db5054a3bab944a489770fa1723752547b6d38
zerpex <zerpex@gmail.com>
2018-01-26 10:30:15 +0100

    [enh] Clean code.

--- files/scripts/app_Syncthing.sh
+++ files/scripts/app_Syncthing.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # Syncthing installation
 
-
 cat files/includes/syncthing.docker >> docker-compose.yml
 
-$SUDO sed -i "s@FQDN@$Sy_SDOM.$DOMAIN@g" docker-compose.yml
-$SUDO sed -i "s@INCOMING@$INC_PATH@g" docker-compose.yml
-$SUDO sed -i "s@MEDIA@$MEDIA_PATH@g" docker-compose.yml
-$SUDO sed -i "s@tool-syncro_Syncthing@$Sy_CNAME@g" docker-compose.yml
+sed -i "s@FQDN@$Sy_SDOM.$DOMAIN@g" docker-compose.yml
+sed -i "s@INCOMING@$INC_PATH@g" docker-compose.yml
+sed -i "s@MEDIA@$MEDIA_PATH@g" docker-compose.yml
+sed -i "s@tool-syncro_Syncthing@$Sy_CNAME@g" docker-compose.yml
 
 # Set Muximux configuration
 cat <<EOF >> files/includes/muximux.conf
@@ -22,6 +21,5 @@ enabled = "true"
 dd = "true"
 EOF
 
-$SUDO sed -i "s@192.168.42.52@$Sy_SDOM.$DOMAIN@g" files/includes/muximux.conf
-
+sed -i "s@192.168.42.52@$Sy_SDOM.$DOMAIN@g" files/includes/muximux.conf
 INSTALLED+=('Sy')
