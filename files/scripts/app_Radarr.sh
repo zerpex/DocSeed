b22c62bf13e160e0be9ca63b3970ad039f5d4c9b
zerpex <zerpex@gmail.com>
2018-01-26 10:29:14 +0100

    [enh] Clean code.

--- files/scripts/app_Radarr.sh
+++ files/scripts/app_Radarr.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # Radarr installation
 
-
-$SUDO mkdir -p $INC_PATH/movies
-$SUDO mkdir -p $MEDIA_PATH/movies
+mkdir -p $INC_PATH/movies
+mkdir -p $MEDIA_PATH/movies
 
 cat files/includes/radarr.docker >> docker-compose.yml
 
-$SUDO sed -i "s@FQDN@$Rd_SDOM.$DOMAIN@g" docker-compose.yml
-$SUDO sed -i "s@INCOMING@$INC_PATH/movies@g" docker-compose.yml
-$SUDO sed -i "s@RMOVIES@$MEDIA_PATH/movies@g" docker-compose.yml
-$SUDO sed -i "s@autodl-movies_radarr@$Rd_CNAME@g" docker-compose.yml
+sed -i "s@FQDN@$Rd_SDOM.$DOMAIN@g" docker-compose.yml
+sed -i "s@INCOMING@$INC_PATH/movies@g" docker-compose.yml
+sed -i "s@RMOVIES@$MEDIA_PATH/movies@g" docker-compose.yml
+sed -i "s@autodl-movies_radarr@$Rd_CNAME@g" docker-compose.yml
 
 # Set Muximux configuration
 cat <<EOF >> files/includes/muximux.conf
@@ -24,6 +23,5 @@ color = "#35c5f4"
 enabled = "true"
 EOF
 
-$SUDO sed -i "s@192.168.42.52@$Rd_SDOM.$DOMAIN@g" files/includes/muximux.conf
-
+sed -i "s@192.168.42.52@$Rd_SDOM.$DOMAIN@g" files/includes/muximux.conf
 INSTALLED+=('Rd')
