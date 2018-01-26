23787fca07c9b87c2d0857ad8eb3832566f5eeb6
zerpex <zerpex@gmail.com>
2018-01-26 10:27:35 +0100

    [enh] Clean code.

--- files/scripts/app_Muximux.sh
+++ files/scripts/app_Muximux.sh
@@ -3,8 +3,8 @@
 
 cat files/includes/muximux.docker >> docker-compose.yml
 
-$SUDO sed -i "s@FQDN@$Mx_SDOM.$DOMAIN@g" docker-compose.yml
-$SUDO sed -i "s@tool-manage_Muximux@$Mx_CNAME@g" docker-compose.yml
-$SUDO sed -i "s@192.168.42.52@$Mx_SDOM.$DOMAIN@g" files/includes/muximux.conf
+sed -i "s@FQDN@$Mx_SDOM.$DOMAIN@g" docker-compose.yml
+sed -i "s@tool-manage_Muximux@$Mx_CNAME@g" docker-compose.yml
+sed -i "s@192.168.42.52@$Mx_SDOM.$DOMAIN@g" files/includes/muximux.conf
 
 INSTALLED+=('Mx')
