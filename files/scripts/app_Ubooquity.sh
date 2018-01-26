64c8bb014dbd462f9e339dd272ac2a6c4a90d165
zerpex <zerpex@gmail.com>
2018-01-26 10:30:48 +0100

    [enh] Clean code.

--- files/scripts/app_Ubooquity.sh
+++ files/scripts/app_Ubooquity.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # Ubooquity installation
 
-
-$SUDO mkdir -p $MEDIA_PATH/library
+mkdir -p $MEDIA_PATH/library
 
 cat files/includes/ubooquity.docker >> docker-compose.yml
 
-$SUDO sed -i "s@FQDN_ADM@$Ua_SDOM.$DOMAIN@g" docker-compose.yml
-$SUDO sed -i "s@FQDN@$Ub_SDOM.$DOMAIN@g" docker-compose.yml
-$SUDO sed -i "s@LIBRARY@$MEDIA_PATH/library@g" docker-compose.yml
-$SUDO sed -i "s@stream-comics_ubooquity@$Ub_CNAME@g" docker-compose.yml
+sed -i "s@FQDN_ADM@$Ua_SDOM.$DOMAIN@g" docker-compose.yml
+sed -i "s@FQDN@$Ub_SDOM.$DOMAIN@g" docker-compose.yml
+sed -i "s@LIBRARY@$MEDIA_PATH/library@g" docker-compose.yml
+sed -i "s@stream-comics_ubooquity@$Ub_CNAME@g" docker-compose.yml
 
 # Set Muximux configuration
 cat <<EOF >> files/includes/muximux.conf
@@ -23,6 +22,5 @@ color = "#3d6fae"
 enabled = "true"
 EOF
 
-$SUDO sed -i "s@192.168.42.52@$Ub_SDOM.$DOMAIN@g" files/includes/muximux.conf
-
+sed -i "s@192.168.42.52@$Ub_SDOM.$DOMAIN@g" files/includes/muximux.conf
 INSTALLED+=('Ub')
