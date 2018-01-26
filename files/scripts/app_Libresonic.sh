def5b501205897b246d7fc6e2aa19055820c6333
zerpex <zerpex@gmail.com>
2018-01-26 10:26:50 +0100

    [enh] Clean code.

--- files/scripts/app_Libresonic.sh
+++ files/scripts/app_Libresonic.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 # Libresonic installation
 
-
-$SUDO mkdir -p $MEDIA_PATH/sound/music
-$SUDO mkdir -p $MEDIA_PATH/sound/podcast
-$SUDO mkdir -p $MEDIA_PATH/sound/playlist
-$SUDO mkdir -p $MEDIA_PATH/sound/other
+mkdir -p $MEDIA_PATH/sound/music
+mkdir -p $MEDIA_PATH/sound/podcast
+mkdir -p $MEDIA_PATH/sound/playlist
+mkdir -p $MEDIA_PATH/sound/other
 
 cat files/includes/libresonic.docker >> docker-compose.yml
 
-$SUDO sed -i "s@FQDN@$Ls_SDOM.$DOMAIN@g" docker-compose.yml
-$SUDO sed -i "s@MUSIC@$MEDIA_PATH/sound/music@g" docker-compose.yml
-$SUDO sed -i "s@PODCASTS@$MEDIA_PATH/sound/podcast@g" docker-compose.yml
-$SUDO sed -i "s@PLAYLISTS@$MEDIA_PATH/sound/playlist@g" docker-compose.yml
-$SUDO sed -i "s@MEDIA@$MEDIA_PATH/sound/other@g" docker-compose.yml
-$SUDO sed -i "s@stream-music_libresonic@$Ls_CNAME@g" docker-compose.yml
+sed -i "s@FQDN@$Ls_SDOM.$DOMAIN@g" docker-compose.yml
+sed -i "s@MUSIC@$MEDIA_PATH/sound/music@g" docker-compose.yml
+sed -i "s@PODCASTS@$MEDIA_PATH/sound/podcast@g" docker-compose.yml
+sed -i "s@PLAYLISTS@$MEDIA_PATH/sound/playlist@g" docker-compose.yml
+sed -i "s@MEDIA@$MEDIA_PATH/sound/other@g" docker-compose.yml
+sed -i "s@stream-music_libresonic@$Ls_CNAME@g" docker-compose.yml
 
 # Set Muximum configuration
 cat <<EOF >> files/includes/muximux.conf
@@ -28,6 +27,5 @@ color = "#cc7b19"
 enabled = "true"
 EOF
 
-$SUDO sed -i "s@192.168.42.52@$Ls_SDOM.$DOMAIN@g" files/includes/muximux.conf
-
+sed -i "s@192.168.42.52@$Ls_SDOM.$DOMAIN@g" files/includes/muximux.conf
 INSTALLED+=('Ls')
