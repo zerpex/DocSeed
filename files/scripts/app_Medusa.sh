84213a8be304c08a9eaf9874b922c943978f35ea
zerpex <zerpex@gmail.com>
2018-01-26 10:27:17 +0100

    [enh] Clean code.

--- files/scripts/app_Medusa.sh
+++ files/scripts/app_Medusa.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # SickGear installation
 
-
-$SUDO mkdir -p $INC_PATH/tv
-$SUDO mkdir -p $MEDIA_PATH/tv
+mkdir -p $INC_PATH/tv
+mkdir -p $MEDIA_PATH/tv
 
 cat files/includes/medusa.docker >> docker-compose.yml
 
-$SUDO sed -i "s@FQDN@$Sg_SDOM.$DOMAIN@g" docker-compose.yml
-$SUDO sed -i "s@INCOMING@$INC_PATH/tv@g" docker-compose.yml
-$SUDO sed -i "s@TVSHOWS@$MEDIA_PATH/tv@g" docker-compose.yml
-$SUDO sed -i "s@autodl-tv_medusa@$Sg_CNAME@g" docker-compose.yml
+sed -i "s@FQDN@$Sg_SDOM.$DOMAIN@g" docker-compose.yml
+sed -i "s@INCOMING@$INC_PATH/tv@g" docker-compose.yml
+sed -i "s@TVSHOWS@$MEDIA_PATH/tv@g" docker-compose.yml
+sed -i "s@autodl-tv_medusa@$Sg_CNAME@g" docker-compose.yml
 
 # Set Muximux configuration
 cat <<EOF >> files/includes/muximux.conf
@@ -24,6 +23,5 @@ color = "#b6f000"
 enabled = "true"
 EOF
 
-$SUDO sed -i "s@192.168.42.52@$Sg_SDOM.$DOMAIN@g" files/includes/muximux.conf
-
+sed -i "s@192.168.42.52@$Sg_SDOM.$DOMAIN@g" files/includes/muximux.conf
 INSTALLED+=('Sg')
