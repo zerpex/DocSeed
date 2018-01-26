33d0fa2340a2c33479bd380736c710f90b96df19
zerpex <zerpex@gmail.com>
2018-01-26 10:32:16 +0100

    [enh] Clean code.

--- files/scripts/ins_docker-compose.sh
+++ files/scripts/ins_docker-compose.sh
@@ -2,7 +2,7 @@
 #
 # This script installs docker-compose
 
-    $SUDO DOCKER_COMPOSE_VERSION="$(curl https://github.com/docker/compose/releases/latest 2> /dev/null | sed 's#.*tag/##;s#">.*##')"
-    $SUDO curl -L https://github.com/docker/compose/releases/download/"$DOCKER_COMPOSE_VERSION"/docker-compose-"$(uname -s)"-"$(uname -m)" > /usr/local/bin/docker-compose
-    $SUDO curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
-    $SUDO chmod +x /usr/local/bin/docker-compose
+    DOCKER_COMPOSE_VERSION="$(curl https://github.com/docker/compose/releases/latest 2> /dev/null | sed 's#.*tag/##;s#">.*##')"
+    curl -L https://github.com/docker/compose/releases/download/"$DOCKER_COMPOSE_VERSION"/docker-compose-"$(uname -s)"-"$(uname -m)" > /usr/local/bin/docker-compose
+    curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
+    chmod +x /usr/local/bin/docker-compose
