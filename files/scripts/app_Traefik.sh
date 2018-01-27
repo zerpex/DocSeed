e4f8d7c901a9fb46aa96d75eeb2418953988a4e1
zerpex <zerpex@gmail.com>
2018-01-27 17:54:11 +0100

    [fix] sed syntax.

--- files/scripts/app_Traefik.sh
+++ files/scripts/app_Traefik.sh
@@ -8,5 +8,5 @@ TRAEFIK_CONF=$CONF_PATH/traefik/traefik.toml
 cp files/includes/traefik.toml $TRAEFIK_CONF
 cat files/includes/traefik.docker >> docker-compose.yml
 
-sed -i "s@EMAIL_CH@$MAIL@g" $TRAEFIK_CONF
+sed -i "s/EMAIL_CH/$MAIL/g" $TRAEFIK_CONF
 sed -i "s@domain.tld@$DOMAIN@g" $TRAEFIK_CONF
